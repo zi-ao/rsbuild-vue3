4f594d04da173c8d2364dce60cb8a1a690dc3819
zi-ao <869822845@qq.com>
2024-11-23 21:49:02 +0800

    git commit 时格式化代码

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,7 +3,7 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
 
-  // biome-ignore lint/complexity/noBannedTypes: reason
+  // biome-ignore lint: .vue 文件支持
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
